e3f208f2c9daab2a5bef09c73f823dd76d7edb30
Priya S <geethapriya.sj@gmail.com>
2025-08-26 00:42:12 +0530

    Initial commit

--- Pycharm_python_code/Break-forloop.py
+++ Pycharm_python_code/Break-forloop.py
@@ -2,6 +2,7 @@ month_Sales = [1200, 1300, 1450, 2000, 1250]
 month_Sales.sort(reverse=True)
 threshold = 1350
 
+
 for(index, sales) in enumerate(month_Sales, start=1):
     if sales < threshold:
         print(f"Sales less than threshold in month {index}: {sales}")
